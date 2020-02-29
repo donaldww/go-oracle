6a43ab1878575dc50ae8df014aa269cca1d60969
Donald Wilson <donaldww@icloud.com>
2020-02-29 05:28:25 -0500

    Fix package name

--- oracle_test.go
+++ oracle_test.go
@@ -1,7 +1,7 @@
 // Copyright 2020 by Donald Wilson. All rights reserved.
 // Use of this source code is governed by an MIT
 // license that can be found in the LICENSE file.
-package go_oracle
+package oracle
 
 import (
 	"fmt"
